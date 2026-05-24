80046b45fff5c9d9fb4b0effda74815110182233
dscastillol <dscastillol@unal.edu.co>
2026-05-24 15:34:53 -0500

    Added query and document

---
+++ app/ingestion.py
@@ -0,0 +1,55 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_postgres import PGVector
+
+
+CONNECTION = (
+    "postgresql+psycopg://postgres:postgres@localhost:5432/ragdb"
+)
+
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+
+def ingest_document(path):
+
+    # cargar PDF
+    loader = PyPDFLoader(path)
+
+    docs = loader.load()
+
+    print(f"Loaded {len(docs)} pages")
+
+
+    # dividir en chunks
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100
+    )
+
+    chunks = splitter.split_documents(docs)
+
+    print(f"Created {len(chunks)} chunks")
+
+
+    # conectar vector store
+    vector_store = PGVector(
+        embeddings=embeddings,
+        connection=CONNECTION,
+        collection_name="documents"
+    )
+
+
+    vector_store.add_documents(chunks)
+
+    print("Documents stored successfully!")
+
+
+if __name__ == "__main__":
+
+    ingest_document(
+        "data/sample.pdf"
+    )
